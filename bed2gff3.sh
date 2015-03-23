303fc581ec28893c2941dc5e2e15cf27826013f2
Surya Saha <ss2489@cornell.edu>
2015-03-23 15:28:47 -0400

    init

---
+++ bed2gff3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Surya Saha
+# BTI/PPath@Cornell
+# Purpose: Write out a GFF3 file for a BED file supplied as parameter
+
+#SL2.50ch03	18370591	18592711	Contig89
+
+#SL2.50ch03	SL2.50_assembly	region	6521993	6866487	.	+	.	ID=1
+
+awk -F"\t" 'BEGIN{OFS="\t"} {print $1,"source","region",$2+1,$3,".","+",".",$4}' "$1"
+
+
+

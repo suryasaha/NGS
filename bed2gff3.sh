cc9b65a2e30060d779077d3cb76ff8d605e9ca2f
Surya Saha <ss2489@cornell.edu>
2015-03-23 15:32:04 -0400

    added header

--- bed2gff3.sh
+++ bed2gff3.sh
@@ -5,9 +5,9 @@
 # Purpose: Write out a GFF3 file for a BED file supplied as parameter
 
 #SL2.50ch03	18370591	18592711	Contig89
-
 #SL2.50ch03	SL2.50_assembly	region	6521993	6866487	.	+	.	ID=1
 
+printf "##gff-version 3\n"
 awk -F"\t" 'BEGIN{OFS="\t"} {print $1,"source","region",$2+1,$3,".","+",".",$4}' "$1"
 
 
